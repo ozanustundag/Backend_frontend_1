84ce95dca37fefaa2677683a4857f4ab92a4f8b3
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-02-08 18:12:30 +0300

    Repository Desenini Anlamak ve Kodlamak

---
+++ NorthwindBackend/Core/DataAcces/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using NLog.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Core.DataAcces
+{
+  public  interface IEntityRepository<T> where T:class,IEntity,new() //t class olmalı demek değil. t referans tip olmalı.
+    {
+        T Get(Expression<Func<T, bool >> filter);
+        IList<T> GetList(Expression<Func<T, bool>> filter= null);
+
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ NorthwindBackend/DataAcces/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAcces;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DataAcces.Abstract
+{
+    public interface IProductDal:IEntityRepository<Product>
+    {
+
+    }
+
+   
+}
