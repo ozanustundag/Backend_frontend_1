6851659143c3766b1f9d59a88d015418a78a907f
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-02-08 13:35:36 +0300

    Entity Katmanını Kodlamaya Başlamak

---
+++ NorthwindBackend/Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+   public class Product :IEntity // default hali internaldır.
+   {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public int QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitInStock { get; set; }
+
+    }
+}
